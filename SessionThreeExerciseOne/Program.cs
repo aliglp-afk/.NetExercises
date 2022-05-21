b13344f56440559dbafdf1416ded8e1205c6267f
Ali Glp <aligolpayegani777@gmail.com>
2022-05-21 16:27:15 +0430

    made it better

--- SessionThreeExerciseOne/Program.cs
+++ SessionThreeExerciseOne/Program.cs
@@ -2,23 +2,28 @@
 namespace SessionThreeExerciseOne{
     class Program{
         public static void Main(string[] args){
-            Console.WriteLine("Input rows");
-            int row=Convert.ToInt32(Console.ReadLine());
-            for(int i=1;i<=row;i++){
-                for(int j=1;j<=row-i;j++){
-                    Console.Write(" ");
+            while(true){
+                Console.WriteLine("Input rows: (q for quit)");
+                string input=Console.ReadLine();    
+                if(input=="q")
+                    break;
+                int row=Convert.ToInt32(input);
+                for(int i=1;i<=row;i++){
+                    for(int j=1;j<=row-i;j++){
+                        Console.Write(" ");
+                    }
+                    for(int j=1;j<=i;j++){
+                        Console.Write(j%10);
+                    }
+                    for(int j=i-1;j>0;j--)
+                    {
+                        Console.Write(j%10);
+                    }
+                    for(int j=1;j<=row-i;j++){
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine();
                 }
-                for(int j=1;j<=i;j++){
-                    Console.Write(j);
-                }
-                for(int j=i-1;j>0;j--)
-                {
-                    Console.Write(j);
-                }
-                for(int j=1;j<=row-i;j++){
-                    Console.Write(" ");
-                }
-                Console.WriteLine();
             }
         }
     }
