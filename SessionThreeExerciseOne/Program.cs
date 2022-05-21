12857ed07d658054145a3bb4637c30fa64468205
Ali Glp <aligolpayegani777@gmail.com>
2022-05-21 12:28:59 +0430

    Repo cleanup and adding exercise s3

---
+++ SessionThreeExerciseOne/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace SessionThreeExerciseOne{
+    class Program{
+        public static void Main(string[] args){
+            for(int i=1;i<=5;i++){
+                for(int j=1;j<=5-i;j++){
+                    Console.Write(" ");
+                }
+                for(int j=1;j<=i;j++){
+                    Console.Write(j);
+                }
+                for(int j=i-1;j>0;j--)
+                {
+                    Console.Write(j);
+                }
+                for(int j=1;j<=5-i;j++){
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
