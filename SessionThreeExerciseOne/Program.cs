f47d5cf1c2f1137bff80078270938b9107aa8a0d
Ali Glp <aligolpayegani777@gmail.com>
2022-05-21 14:07:28 +0430

    added getting rows from the user

--- SessionThreeExerciseOne/Program.cs
+++ SessionThreeExerciseOne/Program.cs
@@ -2,8 +2,10 @@
 namespace SessionThreeExerciseOne{
     class Program{
         public static void Main(string[] args){
-            for(int i=1;i<=5;i++){
-                for(int j=1;j<=5-i;j++){
+            Console.WriteLine("Input rows");
+            int row=Convert.ToInt32(Console.ReadLine());
+            for(int i=1;i<=row;i++){
+                for(int j=1;j<=row-i;j++){
                     Console.Write(" ");
                 }
                 for(int j=1;j<=i;j++){
@@ -13,7 +15,7 @@ namespace SessionThreeExerciseOne{
                 {
                     Console.Write(j);
                 }
-                for(int j=1;j<=5-i;j++){
+                for(int j=1;j<=row-i;j++){
                     Console.Write(" ");
                 }
                 Console.WriteLine();
