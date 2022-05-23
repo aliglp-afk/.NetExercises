11fa546d263ad136dbb4ea1165f9c41df85a5637
Ali Glp <aligolpayegani777@gmail.com>
2022-05-23 12:49:27 +0430

    Added E2

---
+++ SessionThreeSecondExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace SessionThreeSecondExercise
+{
+    class Program{
+        public static void Main(string[] args){
+            System.Console.WriteLine("Input Number: ");
+            int Sum=0;
+            int input=Convert.ToInt32(Console.ReadLine());
+            for(int i=1;i<=input;i+=2){
+                Sum+=i;
+            }
+            Console.WriteLine($"Sum is : {Sum}");
+        }
+    }
+}
