3c4c6cf85dcf25707a44f20114731ffb9c1e5b93
Ali Glp <aligolpayegani777@gmail.com>
2022-05-28 20:27:11 +0430

    Add Exercise Three-Case (Sensetive One)

--- ExerciseThree/Program.cs
+++ ExerciseThree/Program.cs
@@ -18,7 +18,22 @@ namespace ExerciseThree{
                 Console.WriteLine();                 
             }
             while(cki.Key ==ConsoleKey.A);
-
+            //Case Sensetive One
+            // ConsoleKeyInfo cki;
+            // do{
+            //     Console.WriteLine($"Input S1: ");
+            //     string S1=Console.ReadLine();
+            //     Console.WriteLine($"Input S2: ");
+            //     string S2=Console.ReadLine();
+            //     if(S1==null || S2==null || S1=="" || S2=="" || S1==" " || S2==" ")
+            //         Console.WriteLine($"Input was null");
+            //     else
+            //         Console.WriteLine(S1.Contains(S2)?"S2 was in the S1{index : "+S1.IndexOf(S2)+"}":"S2 Not Found");
+            //     Console.Write($"A for again : ");
+            //     cki =Console.ReadKey();   
+            //     Console.WriteLine();                 
+            // }
+            // while(cki.Key ==ConsoleKey.A);
                 
 
 
