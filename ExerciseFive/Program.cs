d40c91a20b1300865c8589827e1077e0c032ed29
Ali Glp <aligolpayegani777@gmail.com>
2022-06-02 20:26:48 +0430

    Add Exercise Five

---
+++ ExerciseFive/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ExerciseFive
+{
+    public class Program
+    {
+        public static void Main(string[]args){
+            List<Employee>employees=new List<Employee>();
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine($"Input Name e{i+1}");
+                string name=Console.ReadLine();
+                Console.WriteLine($"Input WorkingHour e{i+1}");
+                int workingHour=Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Input Salary e{i+1}");
+                int salary=Convert.ToInt32(Console.ReadLine());
+                Employee employee=new Employee(name,workingHour,salary);
+                employees.Add(employee);
+            }
+            Console.WriteLine("{Name}\t{WorkingHour}\t{Salary}\t{DailyWage}\t{MonthlyWage}");
+            for (int i = 0; i < employees.Count; i++)
+            {
+                Console.WriteLine($"{employees[i].GetResult()}");
+            }
+        }
+    }
+}
---
+++ ExerciseFive/Teacher.cs
@@ -0,0 +1,55 @@
+namespace ExerciseFive
+{
+    public class Employee
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        
+        private int _workingHour;
+        public int WorkingHour
+        {
+            get { return _workingHour; }
+            set { _workingHour = value; }
+        }
+        
+        private int _salary;
+        public int Salary
+        {
+            get { return _salary; }
+            set { _salary = value; }
+        }
+
+        private int _monthlyWage;
+        public int MonthlyWage
+        {
+            get { return _monthlyWage; }
+            set { _monthlyWage = value*WorkingHour*30; }
+        }
+        
+        private int _dailyWage;
+        public int DailyWage
+        {
+            get { return _dailyWage; }
+            set { _dailyWage = value*WorkingHour; }
+        }
+        
+
+        public Employee(string name,int workingHour,int salary)
+        {
+            Name=name;
+            WorkingHour=workingHour;
+            Salary=salary;
+            MonthlyWage=salary;
+            DailyWage=salary;
+        }
+
+        public string GetResult(){
+            return $"{Name}\t{WorkingHour}\t{Salary}\t{DailyWage}\t{MonthlyWage}";
+        }
+        
+    }
+}
