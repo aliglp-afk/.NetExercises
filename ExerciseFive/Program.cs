4b6af3ff84888570a5be96bc595bcce3439026be
Ali Glp <aligolpayegani777@gmail.com>
2022-06-02 21:38:15 +0430

    E5 program last touch

--- ExerciseFive/Program.cs
+++ ExerciseFive/Program.cs
@@ -8,17 +8,17 @@
             {
                 Console.WriteLine($"Input Name e{i+1}");
                 string name=Console.ReadLine();
-                Console.WriteLine($"Input WorkingHour e{i+1}");
+                Console.WriteLine($"Input saatKarDarroz e{i+1}");
                 int workingHour=Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine($"Input Salary e{i+1}");
+                Console.WriteLine($"Input Haghzahmeh e{i+1}");
                 int salary=Convert.ToInt32(Console.ReadLine());
                 Employee employee=new Employee(name,workingHour,salary);
                 employees.Add(employee);
             }
-            Console.WriteLine("{Name}\t{WorkingHour}\t{Salary}\t{DailyWage}\t{MonthlyWage}");
+            Console.WriteLine();
             for (int i = 0; i < employees.Count; i++)
             {
-                Console.WriteLine($"{employees[i].GetResult()}");
+                Console.WriteLine($"{employees[i].GetResult()} \n\n");
             }
         }
     }
