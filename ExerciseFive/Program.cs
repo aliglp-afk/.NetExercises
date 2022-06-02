6291109771749131e4d0439f0db4622bc358204f
Ali Glp <aligolpayegani777@gmail.com>
2022-06-02 21:23:45 +0430

    update to 5 inputs in main

--- ExerciseFive/Program.cs
+++ ExerciseFive/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[]args){
             List<Employee>employees=new List<Employee>();
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 5; i++)
             {
                 Console.WriteLine($"Input Name e{i+1}");
                 string name=Console.ReadLine();
