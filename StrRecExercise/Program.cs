6e6c3de4254ef4799ad2e08f169c33a09c721ec3
Ali Glp <Aligolpayegani777@gmail.com>
2022-06-13 01:01:42 +0430

    Add Rec Exercise

---
+++ StrRecExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace StrRecExercise{
+    public class Program
+    {
+        public static string RevStr(string main,int index){
+            if(index!=main.Length-1){
+                return main[main.Length-index-1] + RevStr(main,index+1);
+            }
+            else
+                return main[0].ToString();
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"Input the String: ");
+            string s=Console.ReadLine();
+            Console.WriteLine($"the reverse is \n{RevStr(s,0)}");
+        }
+    }
+}
